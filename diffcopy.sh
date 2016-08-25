8b80f9c7385d3c66fd2c57084caabccb30934167
Jeremy Sharp <jsharp@Ersatz.local>
2016-08-25 12:28:58 +0200

    wahoo

--- diffcopy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#jsharp 2016-08-25
-
-[[ $@ ]] || { printf "\nUsage: $0 /full/path/to/source/dir /comparison/dir /output/dir\n\n"; exit 1; }
-
-diff $1 $2 | sed 's/Only in //g' | sed 's/: //g' | xargs cp -t $3
-
-
-
