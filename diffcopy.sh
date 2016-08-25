8bcd03b489e39c8c9d934c44fcc66493cec71861
Jeremy Sharp <jsharp@Ersatz.local>
2016-08-25 12:36:00 +0200

    wahoo

--- diffcopy.sh
+++ diffcopy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#jsharp 2016-08-25
+#theodric 2016-08-25
 
 [[ $@ ]] || { printf "\nCopies filenames not found in a second directory, compared to a source directory, to a third directory.\nUseful if you fill up a hard drive while copying stuff. Faster and dumber than rsync.\n\n\nUsage: $0 /full/path/to/source/dir /comparison/dir /output/dir\n\n"; exit 1; }
 
