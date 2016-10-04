6cf1982775eb91348498b2dbeaed3fb44503b5f5
Þéodriċ Æðelfriþ <theodric@users.noreply.github.com>
2016-10-04 11:37:30 +0200

    Update shh.sh
    add -o StrictHostKeyChecking=no to disable annoying yes/no prompt for new hosts

--- shh.sh
+++ shh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 ## Never type your SSH password again!
 ## (This is obviously insecure, so be a smart person.)
-spawn ssh $argv -l username
+spawn ssh -o StrictHostKeyChecking=no $argv -l username
 expect "assword:"
 send "Awesomep4ssw0rd!\n"
 interact 
