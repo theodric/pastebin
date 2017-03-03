b2ed4cc450f92aaeb2548c4e97922f9aef03965d
Þéodriċ Æðelfriþ <theodric@users.noreply.github.com>
2017-03-03 22:13:54 +0100

    Update fuckyouonedrive.bat

--- FuckOneDrive/fuckyouonedrive.bat
+++ FuckOneDrive/fuckyouonedrive.bat
@@ -19,3 +19,6 @@ reg delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
 reg delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
 reg import FuckOneDrive1.reg
 reg import FuckOneDrive2.reg
+echo.
+echo You should reboot now. kthxbye
+echo.
