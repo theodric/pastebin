c27316d9a943f16c1e030c924229f54997eae785
Þéodriċ Æðelfriþ <theodric@users.noreply.github.com>
2017-03-03 22:10:56 +0100

    Update fuckyouonedrive.bat

--- FuckOneDrive/fuckyouonedrive.bat
+++ FuckOneDrive/fuckyouonedrive.bat
@@ -1,10 +1,11 @@
 @echo off
 echo If you didn't run this as Administrator, it won't work. Just sayin'
-echo
-echo
+echo.
 echo --== B L I T Z K R I E G ==--
 taskkill /f /im OneDrive.exe
-rem the non-applicable one for the current OS word size simply won't run, so fuck it
+echo I don't care to check what OS wordlength you're running,
+echo so I will just attempt to run both the 32- and 64-bit uninstallers.
+echo The non-applicable one for the current OS simply won't run, so fuck it.
 %SystemRoot%\System32\OneDriveSetup.exe /uninstall
 %SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
 rd "%UserProfile%\OneDrive" /Q /S
@@ -13,5 +14,5 @@ rd "%ProgramData%\Microsoft OneDrive" /Q /S
 rd "C:\OneDriveTemp" /Q /S
 reg delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
 reg delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
-reg import FuckOneDrive1.reg /f
-reg import FuckOneDrive2.reg /f
+reg import FuckOneDrive1.reg
+reg import FuckOneDrive2.reg
