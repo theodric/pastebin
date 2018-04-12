24e2645cb559cbc046b172eff32cc6ecbff3c6e8
Theodric <theodric@users.noreply.github.com>
2018-04-12 18:46:17 +0200

    make script work again

--- lcd.sh
+++ lcd.sh
@@ -3,23 +3,15 @@
 ## This script is derived from the docs at https://learn.adafruit.com/adafruit-pitft-3-dot-5-touch-screen-for-raspberry-pi/backlight
 ## controls Adafruit PiTFT 3.5" 480x320 resistive touchscreen LCD backlight (and compatibles) using PWM
 ## (the varying pwmc values are to avoid an annoying whine on the dim setting)
-## theodric 20160608
+## theodric 20180412
 
-[[ $@ ]] || { printf "\nPlease specify one of these arguments: on/bright, dim, off\n\n" ; exit 1; }
+[[ $@ ]] || { printf "\nPlease specify one of these arguments: on, off\n\n" ; exit 1; }
 
 ARG=${1}
 
-sudo sh -c "echo 'in' > /sys/class/gpio/gpio508/direction" 2> /dev/null;
-gpio -g mode 18 pwm;
-gpio pwmc 100
-
 case "$ARG" in
-   "on") gpio pwmc 1000;gpio -g pwm 18 1023
+   "on") sudo sh -c 'echo "1" > /sys/class/backlight/soc\:backlight/brightness'
 ;;
-   "bright") gpio pwmc 1000;gpio -g pwm 18 1023
-;;
-   "dim") gpio -g pwm 18 650
-;; 
-  "off") gpio -g pwm 18 0
+  "off") sudo sh -c 'echo "0" > /sys/class/backlight/soc\:backlight/brightness'
 ;;
 esac
